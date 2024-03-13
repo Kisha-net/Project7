5dcb1526582910410d2bbc74deda542dc2c84bbc
kisha-net <antoinetteakinyi7@gmail.com>
2024-03-13 08:41:53 +0300

    finished project 7

---
+++ script.js
@@ -0,0 +1,35 @@
+// PROJECT Description:
+/*
+* You are tasked with manipulating a simple webpage using JavaScript.
+* The webpage contains a paragraph (<p>) element with the id paragraph and an empty div element with the id imageContainer.
+* Your goal is to select the paragraph element, change its content to "Hello World", and then create an image element (<img>) with the source attribute set to "image.jpg" and append it to the imageContainer div.
+
+// INSTRUCTIONS
+
+/* Task 1:
+ *Select the paragraph element with the id paragraph and change its content to "Hello World".
+
+/* Task 2:
+* Using JavaScript, create an image element with the source attribute set to "image.jpg".
+
+/* Task 3:
+* Append the created image element to the div element with the id imageContainer.
+
+
+*/
+
+// Write your code here. SUCCESS
+
+// Task 1
+let p = document.getElementById("paragraph");
+p.innerHTML = "Hello World";
+
+// Task 2
+let image = document.createElement("img");
+image.src = "/images/images.jpeg";
+image.style.width = "30%";
+image.style.height = "auto";
+
+// Task 3
+let imageContainer = document.getElementById("imageContainer");
+imageContainer.appendChild(image);
